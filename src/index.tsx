6c6521808fa455fb109a1da30c4d429df2262f4b
Mateusz Kulesza <mateusz@placki.pl>
2021-04-19 11:49:30 +0200

    VirtualDOM vs JSX

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 window.React = React
 window.ReactDOM = ReactDOM
 
+
 interface UserPet {
   id: number;
   name: string;
@@ -24,13 +25,23 @@ const users: User[] = [
   { name: 'Kate', color: 'lightgreen', pets: [{ id: 123, name: 'parrot' }] },
 ]
 
-const UserProfile = (props: { user: User }) => React.createElement('div', {
+const UserProfile2 = (props: { user: User }) => React.createElement('div', {
   id: '123', style: { color: 'black', background: props.user.color, padding: '20px' }
 },
   props.user.pets.map(pet => React.createElement('p', { key: pet.id }, props.user.name + ' have a  ' + pet.name)),
   'Add more : ',
   React.createElement('input')
 )
+
+const UserProfile = (props: { user: User }) => <div id="z123" className={"user-profile"} style={
+  { color: 'black', background: props.user.color, padding: '20px' }
+}>
+  {props.user.pets.map(pet => <p key={pet.id}> {props.user.name} have a {pet.name}</p>)}
+  Add more :
+  <input />
+</div>
+
+debugger;
 
 ReactDOM.render(React.createElement('div', {},
   UserProfile({ user: users[0] }),
