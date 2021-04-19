9f8879c3ea132c47932b5d76ab8fe260edb3f6c4
Mateusz Kulesza <mateusz@placki.pl>
2021-04-19 15:44:14 +0200

    Excercise - Populate list with data, click to select with active class

--- src/playlists/components/PlaylistList.tsx
+++ src/playlists/components/PlaylistList.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 
 interface Props {
-    
+
 }
 
 export const PlaylistList = (props: Props) => {
+
+
     return (
         <div>
-            PlaylistList
+
+            {/* .list-group.list-group-flush>.list-group-item.list-group-item-action*3{Text} */}
+            <div className="list-group list-group-flush">
+                <div className="list-group-item list-group-item-action active">Text</div>
+                <div className="list-group-item list-group-item-action ">Text</div>
+                <div className="list-group-item list-group-item-action">Text</div>
+            </div>
+
         </div>
     )
 }
