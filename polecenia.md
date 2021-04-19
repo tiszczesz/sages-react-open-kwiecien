517b461bc1ed71fc8362b87da0fd3c9acf7e5466
Mateusz Kulesza <mateusz@placki.pl>
2021-04-19 11:01:47 +0200

    React Virtual DOM, props, children, ...  + ReactDOM

---
+++ polecenia.md
@@ -0,0 +1,27 @@
+
+## Instalacje
+node -v
+v14.16.1
+
+npm -v
+6.14.6
+
+code -v
+1.55.2
+3c4e3df9e89829dce27b7b5c24508306b151f30d
+x64
+
+git --version
+git version 2.28.0.windows.1
+
+## Create React App
+npm i -g create-react-app 
+create-react-app --help
+
+cd ..
+create-react-app sages-react-open-kwiecien --template typescript 
+
+cd sages-react-open-kwiecien
+npm start
+npm start -- --port 1234
+
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+window.React = React
+window.ReactDOM = ReactDOM
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const vdiv = React.createElement('div', {
+  id: '123', style: {
+    color: 'black', background: 'hotpink', padding: '20px'
+  }
+},
+  React.createElement('p', {},
+    'Bob ma kota ',
+    React.createElement('input')
+  )
+)
+ReactDOM.render(vdiv, document.getElementById('root'))
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
